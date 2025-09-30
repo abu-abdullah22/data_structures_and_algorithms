43c46fab98c098fefe49ed562f7fd8e888b4c5a7
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-09-30 23:46:35 +0600

    threeSum

---
+++ threeSum.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        n = len(nums)
+        
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i -1]:
+                continue 
+            
+            target = -nums[i]
+            left,right =  i+1, n-1
+
+            
+            while left < right :
+                current_sum = nums[left] + nums[right]
+
+                if current_sum < target:
+                    left += 1 
+                
+                elif current_sum > target:
+                    right -= 1
+                else :
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+                
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -=1
+        
+        return res 
